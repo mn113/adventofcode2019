4013a98e30eada77b6e6995eb78f03b6f160af17
Martin Nicholson <recyclebing+github@gmail.com>
2019-12-05 19:42:52 +0100

    template.py

---
+++ python/template.py
@@ -0,0 +1,13 @@
+def read_input():
+    with open('../inputs/input01.txt') as fp:
+        lines = fp.readlines()
+        return [int(line) for line in lines]
+
+def part1():
+    pass
+
+def part2():
+    pass
+
+part1()
+part2()
