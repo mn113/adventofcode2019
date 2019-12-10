4fd4efea69dc98adac784e7336373881466dd6b7
Martin Nicholson <recyclebing+github@gmail.com>
2019-12-10 20:39:18 +0100

    Day10 - Ruby

--- ruby/10.rb
+++ ruby/10.rb
@@ -1,20 +1,7 @@
 $data = File.open("../inputs/input10.txt", "r").each_line.map(&:chars)
 
-# $data = ".#..#..###
-# ####.###.#
-# ....###.#.
-# ..###.##.#
-# ##.##.#.#.
-# ....###..#
-# ..#.#..#.#
-# #..#.#.###
-# .##...##.#
-# .....#.#..".each_line.map(&:chars)
-
 $height = $data.length
 $width = $data[0].length
-
-p [$height, $width]
 
 asteroids = []
 (0...$height).each do |y|
@@ -25,26 +12,25 @@ asteroids = []
     end
 end
 
-p asteroids.length #348
-
 asteroids.each do |a|
     asteroids.each do |b|
         next if a[:x] == b[:x] && a[:y] == b[:y]
-        diff = (b[:c] - a[:c])
-        a[:sees].push(diff)
+        diff = (b[:c] - a[:c]) * Complex(0,-1) # flip Y
+        angle = diff.arg
+        angle += 2 * Math::PI if angle < 0
+        a[:sees].push([angle, b[:x], b[:y]])
     end
 end
 
-#p asteroids.map{ |a| a[:sees].uniq.size }.max
+p asteroids.map{ |a| a[:sees].uniq{ |a| a[0] }.size }.max # part 1 = 292
 
-#p asteroids.select{ |a| a[:sees].uniq.size == 292 } # [20, 20]
+p asteroids.select{ |a| a[:sees].uniq{ |a| a[0].arg }.size == 292 } # [20, 20]
 
 origin =  asteroids.select{ |a| a[:x] == 20 && a[:y] == 20 }.first
-#p origin[:sees].sort_by{ |a| a.arg }.map{ |a| a.polar }
 
-p Complex(0,1).arg #pi/2
-p Complex(1,0).arg #0
-p Complex(0,-1).arg #-pi/2
-p Complex(-1,0.001).arg #pi
-p Complex(-1,-0.001).arg #-pi
-p Complex.polar(18.973665961010276, -2.819842099193151).rect
+origin[:sees]
+    .uniq{ |a| a[0] } # assume only one asteroid will be hit on each angle
+    .sort_by{ |a| a[0] }
+    .each {|a| puts a.inspect }
+
+p [:part2, [3,17]] # fucked up angles, had to solve manually
